8e4702e775af10f6c9ada6c8a8ffa38e425415db
Amrendra Singh <happysingh0987688@gmail.com>
2025-08-28 22:40:18 +0530

    start

---
+++ lib/models/expense.dart
@@ -0,0 +1,15 @@
+import 'package:uuid/uuid.dart';
+
+const  uuid = Uuid();
+
+class Expense {
+  const Expense({
+    required this.amount,
+    required this.date,
+    required this.title,
+  }) : id=uuid.v4();
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
