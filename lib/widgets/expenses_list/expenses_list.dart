4d2f8f87b62784510decde9a136ca265954c3b9d
Amrendra Singh <happysingh0987688@gmail.com>
2025-09-01 12:23:14 +0530

    making category and display

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,15 +1,16 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses});
 
   final List<Expense> expenses;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (cntx, index) => Text(expenses[index].title),
+      itemBuilder: (cntx, index) => ExpenseItem(expenses[index]),
     );
   }
 }
